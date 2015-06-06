07acf62baea74a341cc3bb289817b216b031e526
lulalachen <lulalachen@gmail.com>
2015-06-06 21:22:22 +0800

    poka

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,7 +2,28 @@ var ngIndex = angular.module('ngIndex', []);
 
 ngIndex.controller('indexCtrl', ['$scope','$timeout', function ($scope, $timeout) {
 	console.log('hi');
+	$('#in').fadeIn();
 
+	$timeout(function() {
+		$('#in').fadeOut();
+		$('#content').fadeIn();
+		console.log('hee')
+	}, 1000);
+
+
+	$scope.start = function(){
+		$scope.startBlock = false;
+		$scope.loginBlock = true;
+	}
+
+	$scope.login = function(app){
+		$('#content').addClass('changePage');
+		$timeout(function() {
+			$scope.loginBlock = false;
+			$scope.friendBlock = true;	
+		}, 1000);
+		
+	}
 
 }])
 
