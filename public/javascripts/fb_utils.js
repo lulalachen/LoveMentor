dd299590aa2daf916cd467764952364be99d6797
Urchin <anabelxrita@gmail.com>
2015-06-07 00:07:44 +0800

    finish fb api

--- public/javascripts/fb_utils.js
+++ public/javascripts/fb_utils.js
@@ -1,6 +1,8 @@
 var accessToken;
 var friendList;
-var selectedId;
+var selfName;
+var selectedName;
+var results;
 
 function FBinit() {
 	FB.init({
@@ -17,7 +19,8 @@ function FBlogin() {
 			accessToken = response.authResponse.accessToken;
      		console.log('Welcome!  Fetching your information.... ');
      		FB.api('/me', function(response) {
-       			console.log('Good to see you, ' + response.name + '.');
+                selfName = response.name;
+       			console.log('Good to see you, ' + selfName + '.');
 				FBqueryFriendList();
      		});
 	    } else {
@@ -33,10 +36,10 @@ function FBlogout() {
 }
 
 function FBqueryFriendList() {
-	FB.api('/me/taggable_friends?limit=1', function(response) {
+	FB.api('/me/taggable_friends', function(response) {
         friendList = [];
         for (var i = 0; i < response.data.length; i++) {
-        	console.log(response.data);
+        	//console.log(response);
         	friendList[i] = [];
             friendList[i][0] = response.data[i].id;
             friendList[i][1] = response.data[i].name;
@@ -49,19 +52,32 @@ function FBqueryFriendList() {
 }
 
 function FBqueryInbox() {
-	FB.api('/me/inbox', function(response) {
-		console.log(response);
-        var selected = document.getElementById('selected_friend');
-
-        var results = '';
+    selectedName = "李佳柔";
+    results = [];
+    FB.api('/me/threads?limit=100', function(response) {
+		//console.log(response);
+        var z = 0;
         for (var i = 0; i < response.data.length; i++) {
-            results += '<div><img src="https://graph.facebook.com/' + response.data[i].id + '/picture">' + response.data[i].name + '</div>';
+            for (var j = 0; j < response.data[i].messages.data.length; j++) {
+                if (response.data[i].messages.data[j].from.name == selectedName) {
+                    results[z] = [];
+                    results[z][0] = 1; //friend
+                    results[z][1] = response.data[i].messages.data[j].created_time;
+                    results[z][2] = response.data[i].messages.data[j].message;
+                    z++;
+                } else if (response.data[i].messages.data[j].from.name == selfName) {
+                    results[z] = [];
+                    results[z][0] = 0; //myself
+                    results[z][1] = response.data[i].messages.data[j].created_time;
+                    results[z][2] = response.data[i].messages.data[j].message;
+                    z++;       
+                }
+            }
         }
-
-        // and display them at our holder element
-        result_holder.innerHTML = '<h2>Result list of your friends:</h2>' + results;
+        console.log(results);
     });
 }
 
 function submitSelectedFriend() {
+    FBqueryInbox();
 }
