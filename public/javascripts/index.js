10f5a4cbbbabb89ae78929dac985d486f84736de
lulalachen <lulalachen@gmail.com>
2015-06-07 07:18:46 +0800

    poka

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,6 @@
 var ngIndex = angular.module('ngIndex', []);
 
-ngIndex.controller('indexCtrl', ['$scope','$timeout', function ($scope, $timeout) {
+ngIndex.controller('indexCtrl', ['$scope','$timeout','$http', function ($scope, $timeout, $http) {
 
 	// Animation //
 	$('#in').fadeIn();
@@ -16,32 +16,11 @@ ngIndex.controller('indexCtrl', ['$scope','$timeout', function ($scope, $timeout
 		name : '',
 		fbId : ''
 	};
-	$scope.friends = [{
-		name : 'Abby Yeh',
-		fbId : '100001234733407'
-	},{
-		name : 'Lulala Chen',
-		fbId : '619160284'
-	},{
-		name : 'Urching Wang',
-		fbId : '100001613672366'
-	},{
-		name : 'Michael Hsiang',
-		fbId : '631572198'
-	},{
-		name : 'Abby Yeh',
-		fbId : '100001234733407'
-	},{
-		name : 'Lulala Chen',
-		fbId : '619160284'
-	},{
-		name : 'Urching Wang',
-		fbId : '100001613672366'
-	},{
-		name : 'Michael Hsiang',
-		fbId : '631572198'
-	}];
-
+	$scope.friends = [];
+	$scope.messages = [];
+	$scope.nextFriend = '';
+	$scope.nextInbox = '';
+	$scope.submitFriend = '';
 
 	var friends = [{
 		name : 'Abby Yeh',
@@ -85,6 +64,7 @@ ngIndex.controller('indexCtrl', ['$scope','$timeout', function ($scope, $timeout
 			       			$scope.currentUser.fbId = response.id;
 							// FBqueryFriendList();
 			     		});
+			     		$scope.getFriends();
 				    } else {
 				    	console.log('User cancelled login or did not fully authorize.');
 				    }
@@ -105,21 +85,139 @@ ngIndex.controller('indexCtrl', ['$scope','$timeout', function ($scope, $timeout
 	}
 
 	$scope.getFriends = function() {
-		FB.api('/me/taggable_friends?limit=1', function(response) {
-	        friendList = [];
+    	$scope.friends = [];
+
+		FB.api('/me/taggable_friends?limit=50', function(response) {
+	        // friendList = [];
+	        console.log(response)
 	        for (var i = 0; i < response.data.length; i++) {
-	        	console.log(response.data);
-	        	friendList[i] = [];
-	            friendList[i][0] = response.data[i].id;
-	            friendList[i][1] = response.data[i].name;
-	            friendList[i][2] = response.data[i].picture.data.url;
+	        	$scope.friends.push({
+	        		fbId : response.data[i].id,
+	        		name : response.data[i].name,
+	        		photo : response.data[i].picture.data.url
+	        	});
+
+	        	//console.log(response);
+	        	// friendList[i] = [];
+	         //    friendList[i][0] = response.data[i].id;
+	         //    friendList[i][1] = response.data[i].name;
+	         //    friendList[i][2] = response.data[i].picture.data.url;
 	            //console.log(friendList[i]);
 	            //friendList[i][2] = "<img src = '" + friendList[i][2] + "'>";
 	            //document.getElementById('result_friends').innerHTML = friendList[i][2];
 	        }
+	        $scope.nextFriend = response.paging.next
+
+	        $scope.moreFriends();
+	    })
+
+	}
+
+	$scope.moreFriends = function() {
+		$http.get($scope.nextFriend).success(function(response){
+			for (var i = 0; i < response.data.length; i++) {
+	        	$scope.friends.push({
+	        		fbId : response.data[i].id,
+	        		name : response.data[i].name,
+	        		photo : response.data[i].picture.data.url
+	        	});
+	        }
+	        $scope.nextFriend = response.paging.next
+		})
+		$timeout(function(){
+	        $scope.moreFriends();
+	    },1000)
+	}
+
+ 	$scope.submit = function(friend) {
+
+ 		$scope.searching = true;
+ 		$('.searchScroll').addClass('flip');
+		$scope.submitFriend = friend;
+		$scope.getInbox(friend.name);
+		$timeout(function() {
+			$scope.searching = false;
+	 		$('.searchScroll').removeClass('flip');
+			console.log(friend)
+		}, 3000);
+		$scope.done = true;
+ 	}
+
+
+	$scope.getInbox = function(name){
+		selectedName = name || "王瘀青";
+	    results = [];
+
+		FB.api('/me/threads?limit=100', function(response) {
+			console.log(response);
+			$scope.messages = [];
+	        var z = 0;	
+	        for (var i = 0; i < response.data.length; i++) {
+	        	if(response.data[i].senders.data.length !== 2){
+	        		console.log('Group chat' + i)
+	        	} else {
+	        		var msg = response.data[i].messages.data ;
+	        		for (var j = 0; j < msg.length; j++){
+	        			if (msg[j].from.name === selectedName) {
+		                    results[z] = [];
+		                    results[z][0] = 1; //friend
+		                    results[z][1] = msg[j].from.id; //Id
+		                    results[z][2] = msg[j].from.name; //Id
+		                    results[z][3] = msg[j].created_time;
+		                    results[z][4] = msg[j].message;
+		                    z++;
+		                } else if (msg[j].from.name === $scope.currentUser.name) {
+		                    results[z] = [];
+		                    results[z][0] = 0; //myself
+		                    results[z][1] = msg[j].to.data[0].id; //Id
+		                    results[z][2] = msg[j].to.data[0].name; //Id
+		                    results[z][3] = msg[j].created_time;
+		                    results[z][4] = msg[j].message;
+		                    z++;       
+		                }
+
+		                if (msg[j].from.name === selectedName && msg[j].message !== '') {
+		                	$scope.messages.push({
+		                		isFriend 		: 1,
+		                		friendId 		: msg[j].from.id,
+		                		friendName 		: msg[j].from.name,
+		                		created_time 	: msg[j].created_time,
+		                		message 		: msg[j].message
+		                	})
+		                } else if (msg[j].from.name === $scope.currentUser.name && msg[j].message !== '') {
+		                    $scope.messages.push({
+		                		isFriend 		: 0,
+		                		friendId 		: msg[j].to.data[0].id,
+		                		friendName 		: msg[j].to.data[0].name,
+		                		created_time 	: msg[j].created_time,
+		                		message 		: msg[j].message
+		                	})  
+		                }
+	        		}
+	        	}
+	        }
 	    });
 	}
 
+	$scope.moreInbox = function() {
+		$http.get($scope.nextInbox).success(function(response){
+			for (var i = 0; i < response.data.length; i++) {
+	        	$scope.friends.push({
+	        		fbId : response.data[i].id,
+	        		name : response.data[i].name,
+	        		photo : response.data[i].picture.data.url
+	        	});
+	        }
+	        $scope.nextInbox = response.paging.next
+		})
+		$timeout(function(){
+	        $scope.moreInbox();
+	    },1000)
+	}
+
+	$scope.processInbox = function() {
+
+	}
 
 }])
 
